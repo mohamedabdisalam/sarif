4d8a227ba877facf7b73740b8c428c870ae87495
Mohamed Abdisalam <mohamed.abdisalam@gmail.com>
2024-10-21 09:48:17 +0100

    Flask set up with quote data

--- currency_converter/app.py
+++ currency_converter/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
-import requests
+from flask import Flask, render_template, request
+from wise_quote import get_wise_quote
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return 'Hello World'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        source_currency = request.form['source_currency'].upper()
+        target_currency = request.form['target_currency'].upper()
+        source_amount = float(request.form['source_amount'])
+
+        quote_data = get_wise_quote(source_currency, target_currency, source_amount)
+        logging.debug(f"Quote Data: {quote_data}")
+
+        return render_template('index.html', quote_data=quote_data)
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- currency_converter/wise_quote.py
+++ currency_converter/wise_quote.py
@@ -1,50 +1,58 @@
 import requests
 import json
+from typing import Dict, Union
+import logging
 
-profile_id = 19555947
+logging.basicConfig(level=logging.DEBUG)
+def get_wise_quote(source_currency: str, target_currency: str, source_amount: float) -> Dict[str, Union[float, str]]:
+    profile_id = 19555947
+    url_quotes = f"https://api.sandbox.transferwise.tech/v3/quotes"
+    token = "3a2d1adf-a85e-4214-aae5-db39b390f3c0"
 
-url_quotes = f"https://api.sandbox.transferwise.tech/v3/quotes"
-token = "3a2d1adf-a85e-4214-aae5-db39b390f3c0"
-
-headers = {
-    'Authorization': f'Bearer {token}',
-    "Content-Type": "application/json"
-}
-
-# sourceCurrency = str(input("Enter in the currency you'd like to convert from: ")).upper()
-# targetCurrency = str(input("Enter in the currency you'd like to convert to: ")).upper()
-# sourceAmount = float(input("Enter the amount you'd like to convert: "))
-
-payload = {
-    "sourceCurrency": "GBP",
-    "targetCurrency": "USD",
-    "sourceAmount": None,
-    "targetAmount": 100,
-    "payOut": None,
-    "preferredPayIn": None,
-    "targetAccount": None,
-    "paymentMetadata": {
-        "transferNature": "MOVING_MONEY_BETWEEN_OWN_ACCOUNTS"
+    headers = {
+        'Authorization': f'Bearer {token}',
+        "Content-Type": "application/json"
     }
-}
+    payload = {
+        "sourceCurrency": source_currency,
+        "targetCurrency": target_currency,
+        "sourceAmount": source_amount,
+        "targetAmount": None,
+        "payOut": None,
+        "preferredPayIn": None,
+        "targetAccount": None,
+        "paymentMetadata": {
+            "transferNature": "MOVING_MONEY_BETWEEN_OWN_ACCOUNTS"
+        }
+    }
+    response = requests.post(
+        url_quotes,
+        headers=headers,
+        data=json.dumps(payload)
+    )
+    if response.status_code == 200:
+        response_data = response.json()
+        logging.basicConfig(level=logging.DEBUG)
+        source_amount = response.json()['paymentOptions'][0]['sourceAmount']
+        wise_fees = response.json()['paymentOptions'][0]['fee']['total']
+        target_amount = response.json()['paymentOptions'][0]['targetAmount']
+        rate = response.json()['rate']
+        total = target_amount / rate
 
+        return {
+            'sourceAmount': source_amount,
+            'wise_fees': wise_fees,
+            'targetAmount': target_amount,
+            'rate': rate,
+        }
+    else:
+        #Handle error case
+        logging.error(f"Error: {response.json()}")
+        return {'error': response.json()}
 
-response = requests.post(
-    url_quotes,
-    headers=headers,
-    data=json.dumps(payload)
-)
-
-# response_data = response.json().option.price.total
-
-sourceAmount = response.json()['paymentOptions'][0]['sourceAmount']
-wise_fees = response.json()['paymentOptions'][0]['fee']['total']
-targetAmount = response.json()['paymentOptions'][0]['targetAmount']
-rate = response.json()['rate']
-total = targetAmount / rate
-print(sourceAmount)
-print(wise_fees)
-print(total)
+        # print(f"Source amount: {sourceAmount}")
+        # print(f"Wise fees: {wise_fees}")
+        # print(f"Tota cost with fees: {total}")
 
 
 
